40403e6a070eff4c7575faad0cf2ca2a88a5f4a9
Euna-kim-1 <euna.kim@edu.sait.ca>
2023-05-19 23:53:05 -0600

    modify app.js code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (  
   <BrowserRouter>
   <Routes>
-  <Route path={process.env.PUBLIC_URL + "/"} element={<Home/>} />
+  {/* <Route path={process.env.PUBLIC_URL + "/"} element={<Home/>} /> */}
+  <Route path="/" element={<Home/>} />
   <Route path="/movie/:id" element={<Detail />} />
   </Routes>
   </BrowserRouter>
