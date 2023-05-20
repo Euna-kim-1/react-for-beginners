852d1e3a0d12d4e382fe547989242497ea4baccc
Euna-kim-1 <euna.kim@edu.sait.ca>
2023-05-19 23:51:10 -0600

    Add a little bit of css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
+//import { BrowserRouter } from 'react-router-dom';
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //  <React.StrictMode>
-  //    <App />
-  //  </React.StrictMode>
-   <BrowserRouter> basename={process.env.PUBLIC_URL}
-  <App />
-   </BrowserRouter>
+    <React.StrictMode>
+      <App />
+   </React.StrictMode>
+  //  <BrowserRouter> basename={process.env.PUBLIC_URL}
+  // <App />
+  //  </BrowserRouter>
 
 );
 
